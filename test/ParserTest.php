6a27d0f73e227d1fd1b639e8b8353c0f4b080ac3
Gaulouis <gaulouis.fr@gmail.com>
2021-02-06 07:28:46 +0100

    Create directory structure and TDD

---
+++ test/ParserTest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Zend\C\Test;
+
+use Zend\C\Parser;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ *
+ */
+class ParserTest extends TestCase {
+    public function testParser() {
+        $this->assertTrue(True);
+    }
+}
